44587542e59cd9be9a52e3774124eba2ddde6e98
Garrett Grimm <garrett@prudentpublishing.com>
2013-10-01 22:47:31 -0400

    adding some content

--- views/footer.php
+++ views/footer.php
@@ -28,5 +28,22 @@
 				<p>Load time: <?php echo self::$loadTime; ?></p>
 			</footer>
 		</div>
+		<?php if (false): ?>
+			<!--Analytics-->
+			<script type="text/javascript">
+
+			  var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', 'UA-1454657-6']);
+			  _gaq.push(['_trackPageview']);
+
+			  setTimeout("_gaq.push(['_trackEvent', '15_seconds', 'read'])", 15000); // To fix 'blog' bounce rates
+
+			  (function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+			</script>		
+		<?php endif ?>		
 	</body>
 </html>
