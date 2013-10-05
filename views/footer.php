39dece354e12551a5c3374038513a28d991e50c7
Garrett Grimm <garrett@prudentpublishing.com>
2013-10-05 14:37:57 -0400

    simplify some stuff

--- views/footer.php
+++ views/footer.php
@@ -1,25 +1,8 @@
 				</div>
 				<div class="col-md-4" id="sidebar">
-					<h2>Wordpress Plugins</h2>
+					<h2>Sidebar Stuff!</h2>
 					<ul>
-						<li><a href="/2011/06/11/wordpress-simple-auto-delay-popup-plugin">Simple Auto Delay Popup Plugin</a></li>
-						<li><a href="/2009/01/29/wordpress-simple-popup-plugin">Simple Popup Plugin</a></li>
-						<li><a href="/2010/02/28/simple-static-google-maps-wordpress-plugin">Simple Static Google Maps</a></li>
-						<li><a href="/2009/11/04/simple-select-all-text-box-wordpress-plugin">Simple Select All Text Box</a></li>
-						<li><a href="/2012/12/20/initial-letter-wordpress-plugin">Initial Letter</a></li>
-					</ul>
-					<h2>Online Music Tools</h2>
-					<ul>
-						<li><a href="http://www.musictheorysite.com/namethatchord/">Name That Chord</a></li>
-						<li><a href="http://www.musictheorysite.com/namethatkey/">Name That Key</a></li>
-						<li><a href="http://www.musictheorysite.com/scale-generator/">Scale Generator</a></li>
-					</ul>
-					<h2>GitHub Projects</h2>
-					<ul>
-						<li><a href="https://github.com/grimmdude/Scale-Generator">Scale Generator</a></li>
-						<li><a href="https://github.com/grimmdude/Raphael-Keyboard">Raphael Keyboard</a></li>
-						<li><a href="https://github.com/grimmdude/Raphael-Guitar">Raphael Guitar</a></li>
-						<li><a href="https://github.com/grimmdude/jQuery-menuFlip">jQuery menuFlip Plugin</a></li>
+						<li><a href="http://google.com">Google</a></li>
 					</ul>
 				</div>
 			</div>
