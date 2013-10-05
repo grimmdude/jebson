d830c1ca77c46e1e6498ad813e4aa3b6fb1f3116
Garrett Grimm <garrett@prudentpublishing.com>
2013-10-05 19:25:50 -0400

    cleanup: :

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 				<div class="col-md-4" id="sidebar">
 					<h2>Sidebar Stuff!</h2>
 					<ul>
-						<li><a href="http://grimmdude.com/2013/08/22/jebson-cms">Jebson Documentation</a></li>
+						<li><a href="http://grimmdude.com/2013/08/22/jebson-cms" target="_blank">Jebson Documentation</a></li>
 					</ul>
 				</div>
 			</div>
