9edc7b6c09dd669cdf10e8e040e032eaea61d7a3
grimmdude <grimmdude@gmail.com>
2013-10-07 17:50:17 -0400

    default view

--- views/footer.php
+++ views/footer.php
@@ -10,23 +10,6 @@
 				<p>Page built with <a href="https://github.com/grimmdude/jebson">Jebson</a></p>
 				<p>Load time: <?php echo self::$loadTime; ?></p>
 			</footer>
-		</div>
-		<?php if (false): ?>
-			<!--Analytics-->
-			<script type="text/javascript">
-
-			  var _gaq = _gaq || [];
-			  _gaq.push(['_setAccount', 'UA-1454657-6']);
-			  _gaq.push(['_trackPageview']);
-
-			  setTimeout("_gaq.push(['_trackEvent', '15_seconds', 'read'])", 15000); // To fix 'blog' bounce rates
-
-			  (function() {
-			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			  })();
-			</script>		
-		<?php endif ?>		
+		</div>	
 	</body>
 </html>
