1bff49f98fe563f800ba2b5f035129540f985650
grimmdude <grimmdude@gmail.com>
2013-10-20 22:01:43 -0400

    clean up post date

--- views/post.php
+++ views/post.php
@@ -1,9 +1,9 @@
 <article>
 	<?php if (!empty(self::$request)): // Don't show title on homepage ?>
 		<h1><?php echo self::$pageData['title']; ?></h1>
-		<?php if (self::$date): ?>
-			<p><?php echo date('m/d/Y', strtotime(self::$date)); ?></p>
-		<?php endif ?>
+	<?php endif ?>
+	<?php if (self::$date): ?>
+		<p><?php echo date('m/d/Y', strtotime(self::$date)); ?></p>
 	<?php endif ?>
 	<?php echo self::$content; ?>
 </article>
