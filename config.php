0fcfe15c3cf3b021c4424cff71f627d5c4f6ce90
Garrett Grimm <garrett@prudentpublishing.com>
2013-12-15 22:48:45 -0500

    config cleanup

--- config.php
+++ config.php
@@ -11,14 +11,13 @@ class Config
 									);
 
 	public static $contentDirectory = 'content/'; // The directory that houses the content files.
-	public static $viewsDirectory = 'views/'; // The directory that houses the view files.
-	public static $viewLoadOrder = array('header','body','footer'); // The order in which main 'structure' views will be loaded
-	public static $homepage = 'home'; // The content file that will serve as your homepage.
-	public static $blogURI = 'blog'; // The URI that will point to your blog (list of posts).
-	public static $wildcards = array(); // Array of wildcards that, when used in a post or page, are replaced by their corresponding value.
-	public static $postsPerPage = 5; // How many posts to show per blog page.
-	public static $cache = false; // Enable/disable cacheing.
-	public static $cacheExclude = array('contact'); // Array of posts/pages that are to be excluded from caching if they have dynamic content.
-	public static $debug = true; // Enable/disable debugging.  Basically toggles PHP error reporting.
-	public static $speedMyShitUp = true; //jk
+	public static $viewsDirectory 	= 'views/'; // The directory that houses the view files.
+	public static $viewLoadOrder 	= array('header','body','footer'); // The order in which main 'structure' views will be loaded
+	public static $homepage 		= 'home'; // The content file that will serve as your homepage.
+	public static $blogURI			= 'blog'; // The URI that will point to your blog (list of posts).
+	public static $wildcards		= array(); // Array of wildcards that, when used in a post or page, are replaced by their corresponding value.
+	public static $postsPerPage 	= 5; // How many posts to show per blog page.
+	public static $cache 			= false; // Enable/disable cacheing.
+	public static $cacheExclude 	= array('contact'); // Array of posts/pages that are to be excluded from caching if they have dynamic content.
+	public static $debug 			= true; // Enable/disable debugging.  Basically toggles PHP error reporting.
 }
