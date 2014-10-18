1b8a6a562ec1911a638b95c58119f646754d723f
Garrett Grimm <ggrimm@prudentpublishing.com>
2014-10-17 23:34:55 -0700

    error reporting off

--- lib/jebson.php
+++ lib/jebson.php
@@ -40,6 +40,10 @@ class Jebson {
 			error_reporting(E_ALL);
 			ini_set('display_errors', '1');
 		}
+		else {
+			error_reporting(0);
+			@ini_set('display_errors', 0);
+		}
 		
 		self::$startTime = microtime(true);
 		self::$pageData = Config::$defaultPageData;
